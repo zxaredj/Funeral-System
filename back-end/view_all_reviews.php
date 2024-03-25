90bcd77db0d3e6939ec284e230dd687596e621e2
zxaredj <118704765+zxaredj@users.noreply.github.com>
2024-03-25 15:43:31 +0800

    update codes

--- back-end/view_all_reviews.php
+++ back-end/view_all_reviews.php
@@ -20,22 +20,25 @@
             width: 80%; /* Adjust as needed */
             padding: 20px;
             margin: 30px 0;
-            background-color: #335889;
+            /* background-color: #335889; */
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            text-align: center;
         }
 
         .review {
             flex: 0 1 calc(33.33% - 20px); /* Each review takes up 1/3 of the container width minus gap */
-            padding: 20px;
+            /* padding: 50px; */
             background-color: #f9f9f9;
             border-radius: 10px;
             box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+            margin: 15px 0px;
+            color: black;
         }
 
         .review p {
             margin: 0;
-            font-size: 16px;
+            font-size: 25px;
             line-height: 1.5;
         }
         .header-title {
@@ -50,8 +53,9 @@
         }
         .filter-container {
             display: flex;
-            justify-content: center;
+            justify-content: right;
             align-items: center;
+            margin: 0px 200px;
         }
         /* Style for the dropdown container */
     .dropdown {
@@ -72,43 +76,56 @@
     }
 
     /* Style for the dropdown arrow */
-    .dropdown select::after {
-        content: '\25BC';
-        font-size: 12px;
-        color: #555;
-        position: absolute;
-        top: 50%;
-        right: 10px;
-        transform: translateY(-50%);
-    }
+.dropdown select::after {
+    content: '\25BC';
+    font-size: 12px;
+    color: #555;
+    position: absolute;
+    top: 50%;
+    right: 10px;
+    transform: translateY(-50%);
+}
 
-    /* Style for the dropdown options */
-    .dropdown select option {
-        padding: 10px 20px;
-    }
+/* Style for the dropdown options */
+.dropdown select option {
+    padding: 10px 20px;
+}
+
+.quote {
+    font-style: italic;
+    color: #144067;
+}
+
+.review-title {
+    text-align: center;
+    margin-top: 120px;
+    margin-bottom: 20px;
+    color: #144067;
+    font-size: 45px;
+}
     </style>
 </head>
 <body>
     <main>
         <section class="homepage-1" id="home">
             <div class="background-image">
-                <img src="../front-end/pictures/homepage-pictures/bg-header.jpg" alt="Background Picture" style="height: 150px;">
+                <!-- <img src="../front-end/pictures/homepage-pictures/bg-header.jpg" alt="Background Picture" style="height: 150px;"> -->
                 <nav>
                     <!-- <img src="/front-end/pictures/pigeon.png" alt="Logo"> -->
                     <ul>
-                        <li><a href="#home">HOME</a></li>
-                        <li><a href="#about">ABOUT</a></li>
-                        <li><a href="#contact">CONTACT</a></li>
-                        <li><a href="#services">SERVICES</a></li>
-                        <li><a href="#planning">PLANNING</a></li>
-                        <li><a href="#obituaries">OBITUARIES</a></li>
-                        <li><a href="signin">SIGN IN</a></li>
+                    <li><a href="../front-end/index.php">HOME</a></li>
+                    <li><a href="../front-end/About Us.html">ABOUT</a></li>
+                    <li><a href="../front-end/services/Service Section.html">SERVICES</a></li>
+                    <li><a href="../front-end/form/form.php">FORM</a></li>
+                    <li><a href="../front-end/planning/planning.php">PLANNING</a></li>
+                    <li><a href="obituaries.php">OBITUARIES</a></li>
+                    <li><a href="../login-signup/signup.html">SIGN IN</a></li>
                     </ul>
                 </nav>
             </div>
         </section>
         <div class="review-page-container">
-            <h1 class="header-title">What our customer reviews</h1>
+            <h1 class="review-title">CUSTOMER REVIEWS</h1>
             <div class="filter-container">
                 <div class="dropdown">
                     <select id="filter-dropdown">
@@ -133,9 +150,9 @@
                             while ($row = mysqli_fetch_assoc($result)) {
                                 // Output review data
                                 echo "<div class='review'>";
-                                echo "<p>Name: " . $row["review_name"]. "</p>";
-                                echo "<p>Date: " . $row["review_date"]. "</p>";
-                                echo "<p>Testimonial: <br>" . $row["review_testimonial"]. "</p>";
+                                echo '<p class="quote">&quot;' . $row["review_testimonial"] . '&quot;</p>'.  "<br>";
+                                echo "<p> " . $row["review_name"]. "</p>";
+                                // echo "<p> " . $row["review_date"]. "</p>";
                                 echo "</div>";
                             }
                         } else {
@@ -145,24 +162,24 @@
                 </div>
             </div>
         </div>
-        <footer>
+        <!-- <footer>
             <div class="footer-container">
                 <div class="footer-item contact-title">
                     <p class="footer-details">Contact Details</p>
                 </div>
                 <div class="footer-item-logo">
                     <img src="../front-end/pictures/homepage-pictures/dove.png" alt="Dove">                  
-                    <p class="footer-details">Philip Sabino Funeral Services</p>
+                    <p class="footer-details">Perpetual Funeral Services</p>
                 </div>
                 <div class="footer-icon">
                     <i class="fa-brands fa-facebook"></i>
                     <i class="fa-solid fa-phone"></i>
                     <i class="fa-brands fa-instagram"></i>
                     <i class="fa-brands fa-square-x-twitter"></i>
-                    <p class="footer-details">92 Interior Sabino Compound, Maysan, Valenzuela, Philippines</p>
+                    <p class="footer-details">511 A. Mabini St., Caloocan City</p>
                 </div>
             </div>
-        </footer>
+        </footer> -->
     </main>
     <script>
         document.addEventListener("DOMContentLoaded", function() {
--- front-end/index.php
+++ front-end/index.php
@@ -13,6 +13,12 @@
     <link href="https://fonts.googleapis.com/css2?family=Inika:wght@700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/53e9ba7f8c.js" crossorigin="anonymous"></script>
 </head>
+<style>
+.quote {
+    font-style: italic;
+    color: #144067;
+}
+</style>
 <body>
     <main>
         <section class="homepage-1" id="home">
@@ -23,11 +29,11 @@
                         <ul>
                             <li><a href="#home">HOME</a></li>
                             <li><a href="About Us.html">ABOUT</a></li>
-                            <li><a href="About Us.html">CONTACT</a></li>
                             <li><a href="services/Service Section.html">SERVICES</a></li>
-                            <li><a href="#planning">PLANNING</a></li>
+                            <li><a href="../front-end/form/form.php">FORM</a></li>
+                            <li><a href="../front-end/planning/planning.php">PLANNING</a></li>
                             <li><a href="../back-end/obituaries.php">OBITUARIES</a></li>
-                            <li><a href="signin">SIGN IN</a></li>
+                            <li><a href="../login-signup/signup.html">SIGN IN</a></li>
                         </ul>
                     </nav>
                     <div class="text-overlay"><p>Where Compassion Meets Comfort <br>
@@ -36,42 +42,40 @@
                 </div>
         </section>
         <section class="homepage-2" id="services">
-            <br>
-            <h1 class="homepage-title">Our Services</h1>
+            <br><br>
+            <h1 class="homepage-title">SERVICES</h1>
             <div class="service-container-outer">
                 <div class="service-container-inner">
                     <div class="service-item">
                         <img src="../front-end/pictures/homepage-pictures/service1.jpg" alt="Burial Services">
                         <p class="service-description">Burial Services</p>
-                        <p class="learn-more">Learn More</p>
+                        <p class="learn-more"><a href="../front-end/services/Burial Services.html" style="text-decoration: none; color: black">Learn More</a></p>
                     </div>
                     <div class="service-item">
                         <img src="../front-end/pictures/homepage-pictures/service2.jpg" alt="Cremation Services">
                         <p class="service-description">Cremation Services</p>
-                        <p class="learn-more">Learn More</p>
+                        <p class="learn-more"><a href="../front-end/services/Cremation Services.html" style="text-decoration: none; color: black">Learn More</a></p>
                     </div>
                     <div class="service-item">
                         <img src="../front-end/pictures/homepage-pictures/service3.jpg" alt="Planning Ahead">
                         <p class="service-description">Planning Ahead</p>
-                        <p class="learn-more">Learn More</p>
+                        <p class="learn-more"><a href="../front-end/planning/planning.php" style="text-decoration: none; color: black">Learn More</a></p>
                     </div>
                     <div class="service-item">
                         <img src="../front-end/pictures/homepage-pictures/service4.jpg" alt="Obituaries Tributes">
                         <p class="service-description">Obituaries Tributes</p>
-                        <p class="learn-more">Learn More</p>
+                        <p class="learn-more"><a href="../back-end/obituaries.php" style="text-decoration: none; color: black">Learn More</a></p>
                     </div>
                 </div>
             </div>
-            <h2 class="homepage-title">Welcome to Philip Sabino Funeral Services</h2>
+            <h2 class="homepage-title">Welcome to Perpetual Funeral Services</h2>
             <p class="homepage-2-description">Discover a compassionate and dignified online experience at our funeral home's website, <br> where we provide support and guidance during difficult times.</p>
-            <hr>
+            <br><br><br><hr>
             <br>
         </section>
         <section class="homepage-3" id="obituaries">
             <br>
-            <br>
-            <br>
-            <h1 class="homepage-title">Recent Obituaries</h1>
+            <h1 class="homepage-title">OBITUARIES</h1>
             <div class="obituaries-container">
                 <div class="obituaries-item obituaries-item-1"></div>
                 <div class="obituaries-item obituaries-item-2"></div>
@@ -79,40 +83,43 @@
                 <div class="obituaries-item obituaries-item-4"></div>
                 <div class="obituaries-item obituaries-item-5"></div>
             </div>
-            <div class="obituaries-info-container">
+           <div class="obituaries-info-container">
                 <div class="obituaries-info">
                     <p class="obituaries-name">Edward M. Norton</p>
-                    <p class="obituaries-date">Feb 27, 1998</p>
+                    <p class="obituaries-date">1998 - 2023</p>
                 </div>
                 <div class="obituaries-info">
                     <p class="obituaries-name">Leon Smith</p>
-                    <p class="obituaries-date">Feb 27, 1998</p>
+                    <p class="obituaries-date">1998 - 2023</p>
                 </div>
                 <div class="obituaries-info">
                     <p class="obituaries-name">Robert S. Brown</p>
-                    <p class="obituaries-date">Mar 2, 1899</p>
+                    <p class="obituaries-date">1998 - 2023</p>
                 </div>
                 <div class="obituaries-info">
                     <p class="obituaries-name">Jennifer A. Jones</p>
-                    <p class="obituaries-date">Apr 7, 2000</p>
+                    <p class="obituaries-date">1998 - 2023</p>
                 </div>
                 <div class="obituaries-info">
                     <p class="obituaries-name">Mary B. Williams</p>
-                    <p class="obituaries-date">Aug 17, 1978</p>
+                    <p class="obituaries-date">1998 - 2023</p>
                 </div>
             </div>
-            <hr style="margin: 0 150px;">
+            <!-- <hr style="margin: 0 150px;"> -->
             <div class="view-container">
-                <button class="original-button" style="margin: 10px 140px 70px 143px; width: 85%;">VIEW ALL OBITUARIES</button>
-            </div>
-            <hr>
+                <button class="original-button" style="margin: 10px auto 80px auto; width: 50%;"><a href="../back-end/obituaries.php" style="text-decoration: none; color: white;">VIEW ALL OBITUARIES</a></button>
+            </div><hr>
+            <!-- <hr>
             <br>
         </section>
         <section class="homepage-4">
-            <h1 class="homepage-title">Our Reviews</h1>
+        <br><br><br> !-->
+        <br><br>
+            <h1 class="homepage-title">REVIEWS</h1>
             <div class="under-title">
-                <div class="stars-container" style="margin-right: 20px;">&#9733;&#9733;&#9733;&#9733;&#9733;</div>
-                <p class="review-description">5.0 rating of 53</p>
+                <br>
+                <!-- <div class="stars-container" style="margin-right: 20px;">&#9733;&#9733;&#9733;&#9733;&#9733;</div>
+                <p class="review-description">5.0 rating of 53</p> -->
             </div>
             <?php 
                 $sql = "SELECT * FROM review LIMIT 4";
@@ -128,10 +135,9 @@
                         // Open review-container div for each review
                         echo '<div class="review-container">';
                         echo '<div class="review">';
-                        echo '<div class="stars-container">&#9733;&#9733;&#9733;&#9733;&#9733;</div>';
-                        echo "<p>" . $row["review_name"]. "</p>";
-                        echo "<p>" . $row["review_date"]. "</p>";
-                        echo "<p>Testimonial: <br>" . $row["review_testimonial"]. "</p>";
+                        echo '<p class="quote">&quot;' . $row["review_testimonial"] . '&quot;</p>'.  "<br>";
+                        echo "<p>" . $row["review_name"]. "</p>" . "<br>"; 
+                        // echo "<p>" . $row["review_date"]. "</p>";
                         echo '</div>'; // Close review div
                         echo '</div>'; // Close review-container div
                     }
@@ -213,12 +219,16 @@
                 </div>
             </div> -->
             <br>
+            <br><br>
             <div class="review-container-footer">
                 <a href="../back-end/view_all_reviews.php" target="_blank" class="original-button" style="margin-right: 30px;">View All Reviews</a>
-                <button class="original-button" id="openReviewFormModal">Write A Review</button>
+
+                <button class="original-button" id="openReviewFormModal"><a href="../login-signup/review.html" style="text-decoration: none; color: white; font-weight: bold;">Write A Review</a></button>
+                
             </div>
+            <!--
             <div class="form-container" id="reviewFormContainer">
-                <div class="close-btn">&times;</div> <!-- Close button (X) -->
+                <div class="close-btn">&times;</div>
                 <div class="form">
                     <h2>Leave A Review</h2>
                     <div class="form-element">
@@ -232,7 +242,7 @@
                     </div>
                     <h3>Rating</h3>
                     <div class="form-footer">
-                        <button>Review Ratings</button>
+                      <button>Review Ratings</button>
                         <button>Submit</button>
                     </div>
                 </div>
@@ -241,31 +251,32 @@
             <br>
             <hr>
             <br>
-        </section>
+        </section> !-->
         <!-- Review Form Modal -->
         
+        <hr>
         <section class="homepage-5" id="contact">
             <br>
             <br>
-            <h1 class="homepage-5-title">Directions</h1>
+            <h1 class="homepage-5-title">DIRECTIONS</h1>
             <div class="wrapper-container">
                 <div class="left-wrapper-container">
                     <div class="left-wrapper-item">
-                        <h2 style="color: #282D32;">Philip Sabino Funeral Services</h2>
+                        <!-- <h2 style="color: black;">Perpetual Funeral Services</h2> -->
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.0610417880775!2d120.96950667590495!3d14.652476375802863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b5ccc6d9badd%3A0x7e0ea7cff476121!2sFloresco%20Building%2C%20A.%20Mabini%20St%2C%20Dagat-Dagatan%2C%20Caloocan%2C%20Metro%20Manila!5e0!3m2!1sfil!2sph!4v1709841733811!5m2!1sfil!2sph" width="100%" height="400px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                        <p class="text-description" style="font-size: 20px;">"Compassionate care in your time of need. Honoring lives with dignity and <br> respect."</p>
+                        <!-- <p class="text-description" style="font-size: 20px; color: black;">"Compassionate care in your time of need. Honoring lives with dignity and <br> respect."</p> -->
                     </div>
                 </div>
                 <div class="right-wrapper-container">
                     <div class="right-wrapper-item">
-                        <h2 style="font-size: 40px;">GET IN TOUCH</h2>
+                        <h2 style="font-size: 40px; color: black;">GET IN TOUCH</h2>
                         <div class="form-container">
                             <form action="">
-                                <input type="name" class="name-input input" placeholder="Enter Your Name:">
+                                <input type="name" class="name-input input" placeholder=" Juan Dela Cruz">
                                 <br>
-                                <input type="email" class="email-input input" placeholder="Enter a valid Email Address:">
+                                <input type="email" class="email-input input" placeholder=" perpetualfuneralservices@gmail.com">
                                 <br>
-                                <textarea class="message-inquiry input" name="message-inquiry" id="message-inquiry" placeholder="Enter your message:"></textarea>
+                                <textarea class="message-inquiry input" name="message-inquiry" id="message-inquiry" placeholder=" Enter your message"></textarea>
                                 <br>
                                 <br>
                                 <button class="btn-submit" value="Submit">SUBMIT</button>
@@ -275,7 +286,7 @@
                 </div>
             </div>
         </section>
-        <section class="homepage-6" id="about">
+        <!-- <section class="homepage-6" id="about">
             <br>
             <h1 class="homepage-title">Directions</h1>
             <div class="map-container">
@@ -285,25 +296,26 @@
                     <p style="margin: 10px 0; text-align: center;">"Compassionate care in your time of need. Honoring lives with dignity and respect."</p>
                 </div>
             </div>
-            <br>
-            <footer>
+            <br> -->
+            
+            <!-- <footer>
                 <div class="footer-container">
                     <div class="footer-item contact-title">
                         <p class="footer-details">Contact Details</p>
                     </div>
                     <div class="footer-item-logo">
                         <img src="pictures/homepage-pictures/dove.png" alt="Dove">                  
-                        <p class="footer-details">Philip Sabino Funeral Services</p>
+                        <p class="footer-details">Perpetual Funeral Services</p>
                     </div>
                     <div class="footer-icon">
                         <i class="fa-brands fa-facebook"></i>
                         <i class="fa-solid fa-phone"></i>
                         <i class="fa-brands fa-instagram"></i>
                         <i class="fa-brands fa-square-x-twitter"></i>
-                        <p class="footer-details">92 Interior Sabino Compound, Maysan, Valenzuela, Philippines</p>
+                        <p class="footer-details">511 A. Mabini St., Caloocan City</p>
                     </div>
                 </div>
-            </footer>
+            </footer> -->
         </section>
     </main>
 </body>
