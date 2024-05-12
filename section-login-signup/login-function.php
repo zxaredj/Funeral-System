dfdaa6e0e517f8c571dd2f0cd0f1b9b3b2f3c5da
navarosaanj <143917561+navarosaanj@users.noreply.github.com>
2024-05-12 16:53:42 +0800

    Client Inbox

--- section-login-signup/login-function.php
+++ section-login-signup/login-function.php
@@ -1,41 +1,32 @@
 <?php
-// session_start();    
+session_start();    
 
-    include '../database/config.php';
+include '../database/config.php';
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        
-        $username = mysqli_real_escape_string($connection, $_POST['username']);
-        $password = mysqli_real_escape_string($connection, $_POST['password']);
-        
-        $stmt = $connection->prepare("SELECT * FROM login WHERE username = ?");
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-        $result = $stmt->get_result();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
     
-        if ($user = $result->fetch_assoc()) {
+    $username = mysqli_real_escape_string($connection, $_POST['username']);
+    $password = mysqli_real_escape_string($connection, $_POST['password']);
+    
+    $stmt = $connection->prepare("SELECT * FROM login WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-            if (password_verify($password, $user['password'])) {
-                $_SESSION['username'] = $username;
-                header("Location: ../section-home/index.php");
-                exit();
-            } else {
-                header("Location: login.php?error=invalid_credentials");
-                exit();
-            }
+    if ($user = $result->fetch_assoc()) {
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['username'] = $username; // Set username in session
+            header("Location: ../section-home/index.php");
+            exit();
         } else {
             header("Location: login.php?error=invalid_credentials");
             exit();
         }
-        $_SESSION['username'] = $username;  // Set username in session
-
-        // Redirect to the originally intended page or default to a specific page
-        $redirect_url = $_SESSION['redirect_url'] ?? 'default-page.php';
-        header("Location: $redirect_url");
+    } else {
+        header("Location: login.php?error=invalid_credentials");
         exit();
     }
-
-
+}
 ?>
